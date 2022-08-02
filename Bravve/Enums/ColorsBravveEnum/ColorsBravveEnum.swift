a1bec7dcd9e368be511fe57e498dc9a21652f5c5
Daniel <daniel.oliveira@foursys.com.br>
2022-08-02 11:06:21 -0300

    [BRAV-0000] Created enum for Color assets of bravve

--- Bravve/Enums/ColorsBravveEnum/ColorsBravveEnum.swift
+++ Bravve/Enums/ColorsBravveEnum/ColorsBravveEnum.swift
@@ -1,8 +1,37 @@
 //
 //  ColorsBravveEnum.swift
-//  Bravve
+//  Teste
 //
 //  Created by Josicleison on 02/08/22.
 //
 
-import Foundation
+//Enum for assets on app
+
+public enum ColorsBravve: String {
+
+    case background = "background"
+    case backgroundBravve = "backgroundBravve"
+    case blue = "blueNav"
+    case borderBottomTexfield = "borderBottomTexfield"
+    case boxOffice = "boxOffice"
+    case buttonGray = "buttonGray"
+    case buttonPink = "buttonPink"
+    case buttonRecoveryPassword = "buttonRecoveryPassword"
+    case buttonUnderline = "buttonUnderline"
+    case cards = "cards"
+    case color = "Color"
+    case coolWorking = "coolWorking"
+    case coWorking = "coWorking"
+    case grayAlertLabel = "grayAlertLabel"
+    case greenAlertLabel = "greenAlertLabel"
+    case hotel = "hotel"
+    case label = "label"
+    case textFieldLabel = "labelTextField"
+    case redAlertLabel = "redAlertLabel"
+    case reserved = "reserved"
+    case reservedCancel = "reservedCancel"
+    case searchBar = "searchBar"
+    case textField = "textField"
+    case textFieldBackground = "textFieldBackground"
+    case textFieldBorder = "textFieldBorder"
+}
