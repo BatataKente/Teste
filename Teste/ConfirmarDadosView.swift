2f7e8902b561bd3ef25a4c74a6caf2630cb0ce2e
helogomes <105743745+helogomes@users.noreply.github.com>
2022-08-03 06:19:28 -0700

    ajustes

--- Teste/ConfirmarDadosView.swift
+++ Teste/ConfirmarDadosView.swift
@@ -10,33 +10,35 @@ import UIKit
 
 class ConfirmarDadosView: UIViewController {
     
-    let backgroundImage1: UIImageView = {
+    private let backgroundImage1: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "wayCell")
+        image.image = UIImage(named: ImagesBravve.wayCell.rawValue)
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
-    let backgroundImage2: UIImageView = {
+    private let backgroundImage2: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "wayConfirm")
+        image.image = UIImage(named: ImagesBravve.wayConfirm.rawValue)
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
     
-    let buttonBack = UIButton()
+    private let buttonBack = UIButton()
     
-    let imageLogo = UIImageView()
+    private let imageLogo = UIImageView()
     
     private lazy var progressBarStackView: (stack: UIStackView,
                                             buttons: [UIButton]) = {
         
-        let buttons = createProgressBarButtons(["userGray",
-                                                "cellGray",
-                                                "emailGray",
-                                                "padlockGray",
-                                                "pencilBlue"])
+        let buttons = createProgressBarButtons([
+                                                IconsBravve.userGray.rawValue,
+                                                IconsBravve.cellGray.rawValue,
+                                                IconsBravve.emailGray.rawValue,
+                                                IconsBravve.padlockGray.rawValue,
+                                                IconsBravve.pencilBlue.rawValue
+                                               ])
         let stackView = UIStackView(arrangedSubviews: buttons)
         stackView.spacing = 7
         
@@ -46,112 +48,112 @@ class ConfirmarDadosView: UIViewController {
     
     
     
-    let label: UILabel = {
+    private let label: UILabel = {
         let label = UILabel()
         label.text = "Bravo! Que tal revisar suas informações, aqui você pode alterá-las!"
         label.textAlignment = .center
-        label.font = UIFont(name: "Ubuntu-Light", size: 16)
-        label.textColor = UIColor(named: "label")
+        label.font = UIFont(name: FontsBravve.light.rawValue, size: CGFloat(16).generateSizeForScreen)
+        label.textColor = UIColor(named: ColorsBravve.label.rawValue)
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let contentView: UIView = {
-      let view = UIView()
-      view.backgroundColor = UIColor(named: "background")
-      view.translatesAutoresizingMaskIntoConstraints = false
-      return view
+    private let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: ColorsBravve.background.rawValue)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
-    let labelName: UILabel = {
+    private let labelName: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Ubuntu-Medium", size: 15)
-        label.textColor = UIColor(named: "label")
+        label.font = UIFont(name: FontsBravve.medium.rawValue, size: CGFloat(15).generateSizeForScreen)
+        label.textColor = UIColor(named: ColorsBravve.label.rawValue)
         //label.text = "Ana Maria Silva"
         label.adjustsFontSizeToFitWidth = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let labelCell: UILabel = {
+    private let labelCell: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Ubuntu-Medium", size: 15)
-        label.textColor = UIColor(named: "label")
+        label.font = UIFont(name: FontsBravve.medium.rawValue, size: CGFloat(15).generateSizeForScreen)
+        label.textColor = UIColor(named: ColorsBravve.label.rawValue)
        // label.text = "11 99686 2647"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let labelEmail: UILabel = {
+    private let labelEmail: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Ubuntu-Medium", size: 15)
-        label.textColor = UIColor(named: "label")
+        label.font = UIFont(name: FontsBravve.medium.rawValue, size: CGFloat(15).generateSizeForScreen)
+        label.textColor = UIColor(named: ColorsBravve.label.rawValue)
        // label.text = "teste@bravve.com.br"
         label.adjustsFontSizeToFitWidth = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    lazy var editButton1: UIButton = {
+    private lazy var editButton1: UIButton = {
         let button = myButton()
         return button
     }()
-    lazy var editButton2: UIButton = {
+    private lazy var editButton2: UIButton = {
         let button = myButton()
         return button
     }()
-    lazy var editButton3: UIButton = {
+    private lazy var editButton3: UIButton = {
         let button = myButton()
         return button
     }()
-    
-    lazy var stackViewName: UIStackView = {
+        
+    private lazy var stackViewName: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [labelName, editButton1])
         stackView.alignment = .fill
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
-        stackView.backgroundColor = .white
+        stackView.backgroundColor = UIColor(named: ColorsBravve.cards.rawValue)
         stackView.layer.borderWidth = 1
         stackView.layer.cornerRadius = 8
-        stackView.layer.borderColor = UIColor(named: "textFieldBorder")?.cgColor
+        stackView.layer.borderColor = UIColor(named: ColorsBravve.textFieldBorder.rawValue)?.cgColor
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 20)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
         }()
     
-    lazy var stackViewCell: UIStackView = {
+    private lazy var stackViewCell: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [labelCell, editButton2])
         stackView.alignment = .fill
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
-        stackView.backgroundColor = .white
+        stackView.backgroundColor = UIColor(named: ColorsBravve.cards.rawValue)
         stackView.layer.borderWidth = 1
         stackView.layer.cornerRadius = 8
-        stackView.layer.borderColor = UIColor(named: "textFieldBorder")?.cgColor
+        stackView.layer.borderColor = UIColor(named: ColorsBravve.textFieldBorder.rawValue)?.cgColor
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 20)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
         }()
     
-    lazy var stackViewEmail: UIStackView = {
+    private lazy var stackViewEmail: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [labelEmail, editButton3])
         stackView.alignment = .fill
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
-        stackView.backgroundColor = .white
+        stackView.backgroundColor = UIColor(named: ColorsBravve.cards.rawValue)
         stackView.layer.borderWidth = 1
         stackView.layer.cornerRadius = 8
-        stackView.layer.borderColor = UIColor(named: "textFieldBorder")?.cgColor
+        stackView.layer.borderColor = UIColor(named: ColorsBravve.textFieldBorder.rawValue)?.cgColor
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 20)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
         }()
     
-    lazy var stackViewLabels: UIStackView = {
+    private lazy var stackViewLabels: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [stackViewName, stackViewCell, stackViewEmail])
         stackView.alignment = .fill
         stackView.axis = .vertical
@@ -160,12 +162,12 @@ class ConfirmarDadosView: UIViewController {
         return stackView
         }()
     
-    let buttonContinue: UIButton = {
+    private let buttonContinue: UIButton = {
         let button = UIButton()
         button.setTitle("Continuar", for: .normal)
-        button.titleLabel?.textColor = .white
-        button.titleLabel?.font = UIFont(name: "Ubuntu-Bold", size: 16)
-        button.backgroundColor = UIColor(named: "buttonPink")
+        button.titleLabel?.textColor = UIColor(named: ColorsBravve.cards.rawValue)
+        button.titleLabel?.font = UIFont(name: FontsBravve.bold.rawValue, size: CGFloat(16).generateSizeForScreen)
+        button.backgroundColor = UIColor(named: ColorsBravve.buttonPink.rawValue)
         button.layer.cornerRadius = 12
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -179,6 +181,7 @@ class ConfirmarDadosView: UIViewController {
         view.addSubviews([backgroundImage2, buttonBack, imageLogo, progressBarStackView.stack, label, contentView])
         contentView.addSubviews([backgroundImage1, stackViewLabels, buttonContinue])
         
+        defaults()
         addConstraints()
         addTargets()
       
@@ -187,20 +190,22 @@ class ConfirmarDadosView: UIViewController {
     
     func myButton() -> UIButton {
         let button = UIButton()
-        button.setImage(UIImage(named: "edit-blue"), for: .normal)
+        button.setImage(UIImage(named: IconsBravve.edit_blue.rawValue), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }
     
-    func addConstraints() {
+    private func defaults() {
         
         buttonBack.setToBackButtonDefault(.backPink)
         imageLogo.setLogoToDefault()
         
+    }
+    
+    private func addConstraints() {
+        
         NSLayoutConstraint.activate([
         
-            
-            
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
             label.topAnchor.constraint(equalTo: progressBarStackView.stack.bottomAnchor, constant: 50),
@@ -242,7 +247,7 @@ class ConfirmarDadosView: UIViewController {
         backgroundImage2.widthAnchorInSuperview(280)
     }
     
-    func addTargets() {
+    private func addTargets() {
         
         buttonBack.addTarget(self, action: #selector(actionButtonBack), for: .touchUpInside)
         
@@ -299,4 +304,5 @@ class ConfirmarDadosView: UIViewController {
         print("tela email")
     }
 }
+
 
