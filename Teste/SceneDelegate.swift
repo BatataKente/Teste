f6eb9bce553064c8477fc37b243e811505547ff5
Daniel <daniel.oliveira@foursys.com.br>
2022-08-03 14:24:37 -0300

    Aprimorada responsividade da progressBar

--- Teste/SceneDelegate.swift
+++ Teste/SceneDelegate.swift
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //
 //        window.rootViewController = tabBarController
         
-        window.rootViewController = MinhasReservas()
+        window.rootViewController = FotoView()
         window.makeKeyAndVisible()
         
         self.window = window
