375381ae6cc67b652a28ba73acaec218ecb07bcb
Lucas Galvao <lukedbzgalvao@gmail.com>
2022-08-05 05:58:04 -0700

    Tela de leitura de qrcode

--- Teste/SceneDelegate.swift
+++ Teste/SceneDelegate.swift
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //
 //        window.rootViewController = tabBarController
         
-        window.rootViewController = OpenDetailsView()
+        window.rootViewController = CheckInQrCodeViewController()
         window.makeKeyAndVisible()
         
         self.window = window
