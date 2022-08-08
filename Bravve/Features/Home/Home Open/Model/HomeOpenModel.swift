17cb48a86da95f1ebe6ccd79b20d82a42f5a0f4f
Evandro Rodrigo Minamoto <evandro.minamoto@foursys.com.br>
2022-08-08 20:06:36 +0000

    [BRAV-0000] Develop

--- Bravve/Features/Home/Home Open/Model/HomeOpenModel.swift
+++ Bravve/Features/Home/Home Open/Model/HomeOpenModel.swift
@@ -5,4 +5,16 @@
 //  Created by user218260 on 7/15/22.
 //
 
-import Foundation
+import UIKit
+
+struct ReserveData {
+    
+    let title: String,
+        description: String,
+        image: UIImage,
+        photoTitle: String,
+        name: String,
+        subName: String,
+        price: String,
+        details: String
+}
