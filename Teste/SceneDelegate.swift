ad6d435357ca00881ca6c3f265bd46cbb4255585
Daniel <Daniell.dam2000msn@gmail.com>
2022-08-08 05:39:23 -0700

    Commando 70% da tela

--- Teste/SceneDelegate.swift
+++ Teste/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-        let vc1 = CheckOutScreen()
+        let vc1 = DetalhesDoEspacoView()
         vc1.tabBarItem.image = UIImage(named: "locationGray")
 
         let vc2 = NomeView()
