eccf22ddb59bdb71a6b4a5a055902a582986445a
Evandro Rodrigo Minamoto <evandro.minamoto@foursys.com.br>
2022-08-08 19:40:37 +0000

    [BRAV-0000] Develop

--- Bravve/Features/Registration/Name/ViewModel/NameViewModel.swift
+++ Bravve/Features/Registration/Name/ViewModel/NameViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  NomeViewModel.swift
+//  NomeView.swift
 //  Bravve
 //
 //  Created by user218260 on 7/15/22.
@@ -31,24 +31,14 @@ class NomeViewModel {
     
     func changeScreenWithProgressBar(_ sender: UIButton) {
         
-        switch sender.configuration?.image {
+        switch sender.currentImage {
             
-            case UIImage(named: "userBlue"):
-            
-                makeNameScreen()
-                stage = .first
-            
-            case UIImage(named: "userGray"):
+            case UIImage(named: IconsBravve.userGray.rawValue):
             
                 makeNameScreen()
                 stage = .first
             
-            case UIImage(named: "cellBlue"):
-            
-                makePhoneScreen()
-                stage = .second
-            
-            case UIImage(named: "cellGray"):
+            case UIImage(named: IconsBravve.cellGray.rawValue):
             
                 makePhoneScreen()
                 stage = .second
@@ -114,72 +104,60 @@ class NomeViewModel {
         }
     }
     
-    private func makeNameScreen() {
+    func makeNameScreen() {
         
         delegate?.setIshidden(leftStackView: true,
                               ddiChoseLabel: false,
                               ways: [true, true, false])
         
-        delegate?.setFont(font: UIFont.systemFont(ofSize: 15))
+        delegate?.setFont(font: UIFont(name: FontsBravve.light.rawValue,
+                                       size: CGFloat(15).generateSizeForScreen) ?? UIFont())
         
         delegate?.setText(rightLabel: "Nome Completo",
                           rightTextField: "",
                           infoLabel: "Para começarmos a conversar, pode nos contar seu nome e sobrenome!")
         
-        delegate?.setProgressBar(personalDataTitle: " Dados pessoais",
-                                 personalDataImage: "userBlue",
-                                 phoneNumberTitle: "",
-                                 phoneNumberImage: "cellGray",
-                                 emailTitle: "",
-                                 emailImage: "emailGray")
+        delegate?.setProgressBar(buttons: [false, true, true, false, true, false])
         
         delegate?.freezeButton()
         
         delegate?.setKeyboardType(keyboardType: .namePhonePad)
     }
     
-    private func makePhoneScreen() {
+    func makePhoneScreen() {
         
         delegate?.setIshidden(leftStackView: false,
                               ddiChoseLabel: true,
                               ways: [true, false, true])
         
-        delegate?.setFont(font: UIFont.systemFont(ofSize: 15))
+        delegate?.setFont(font: UIFont(name: FontsBravve.light.rawValue,
+                                       size: CGFloat(15).generateSizeForScreen) ?? UIFont())
         
         delegate?.setText(rightLabel: "Telefone",
                           rightTextField: "",
                           infoLabel: "Precisamos do seu telefone com DDD!\n Por favor, informe o seu país também.")
         
-        delegate?.setProgressBar(personalDataTitle: "",
-                                 personalDataImage: "userGray",
-                                 phoneNumberTitle: " Celular",
-                                 phoneNumberImage: "cellBlue",
-                                 emailTitle: "",
-                                 emailImage: "emailGray")
+        delegate?.setProgressBar(buttons: [true, false, false, true, true, false])
         
         delegate?.freezeButton()
         
         delegate?.setKeyboardType(keyboardType: .numberPad)
     }
     
-    private func makeEmailScreen() {
+    func makeEmailScreen() {
         
         delegate?.setIshidden(leftStackView: true,
                               ddiChoseLabel: false,
                               ways: [false, true, true])
         
-        delegate?.setFont(font: UIFont.systemFont(ofSize: 15))
+        delegate?.setFont(font: UIFont(name: FontsBravve.light.rawValue,
+                                       size: CGFloat(15).generateSizeForScreen) ?? UIFont())
         
         delegate?.setText(rightLabel: "E-mail",
                           rightTextField: "",
                           infoLabel: "Qual seu email? Não se preocupe, não vamos encher sua caixa de entrada.")
         
-        delegate?.setProgressBar(personalDataTitle: "",
-                                 personalDataImage: "userGray",
-                                 phoneNumberTitle: "",
-                                 phoneNumberImage: "cellGray",
-                                 emailTitle: " Email",
-                                 emailImage: "emailBlue")
+        delegate?.setProgressBar(buttons: [true, false, true, false, false, true])
         
         delegate?.freezeButton()
         
@@ -199,12 +177,7 @@ protocol NomeViewModelProtocol {
                  rightTextField: String,
                  infoLabel: String)
     
-    func setProgressBar(personalDataTitle: String,
-                        personalDataImage: String,
-                        phoneNumberTitle: String,
-                        phoneNumberImage: String,
-                        emailTitle: String,
-                        emailImage: String)
+    func setProgressBar(buttons: [Bool])
     
     func freezeButton()
     
