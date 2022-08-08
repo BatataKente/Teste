572ba254c07ec36febe3246505fca10eacb5a0c4
Lucas Ussier Bitencourt Galvão <lucas.galvao@foursys.com.br>
2022-08-08 21:05:41 +0000

    [BRAV-0000] Check-In View, QRCode reader view.

---
+++ Bravve/Features/Home/Filter/View/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  Bravve
+//
+//  Created by Evandro Rodrigo Minamoto on 08/08/22.
+//
+
+import UIKit
+
+class FilterScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        view.setToDefaultBackgroundColor()
+        
+        super.viewDidLoad()
+    }
+}
