b76cd59fca99abbedb6b30be1b491163213fc83c
Evandro Rodrigo Minamoto <evandro.minamoto@foursys.com.br>
2022-08-09 20:02:31 +0000

    [BRAV-0000] Develop

---
+++ Bravve/Extensions/CustomBarWithFilter.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBarWithFilter.swift
+//  Bravve
+//
+//  Created by Evandro Rodrigo Minamoto on 09/08/22.
+//
+
+import UIKit
+
+struct CustomBarWithFilter {
+    
+    let leftButton: UIButton
+    let stateLabel: UILabel
+    let stateChosedLabel: UILabel
+    let rightButton: UIButton
+    let cityLabel: UILabel
+    let cityChosedLabel: UILabel
+}
---
+++ Bravve/Models/SpacesV1Model.swift
@@ -0,0 +1,8 @@
+//
+//  SpacesV1Model.swift
+//  Bravve
+//
+//  Created by Evandro Rodrigo Minamoto on 09/08/22.
+//
+
+import Foundation
