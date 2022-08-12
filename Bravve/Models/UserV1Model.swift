3cc6d86749922b36f14171f0d7acfa3c11eef205
Pedro Ivo Cardoso de Aquino <pedro.aquino@foursys.com.br>
2022-08-12 12:22:05 +0000

    [BRAV-0000] Feature/api models

--- Bravve/Models/UserV1Model.swift
+++ Bravve/Models/UserV1Model.swift
@@ -6,3 +6,83 @@
 //
 
 import Foundation
+
+struct Hobbies: Codable {
+    var hobbie: Int?
+    var name: String?
+}
+
+struct Interests: Codable {
+    var interest: Int?
+    var name: String?
+}
+
+struct Occupations: Codable {
+    var occupation: Int?
+    var name: String?
+}
+
+struct WorkModels: Codable {
+    var id: Int?
+    var name: String?
+}
+
+struct Amount: Codable {
+    var amount: Int?
+}
+
+struct User: Codable {
+    var name: String?
+    var phone_number: String?
+    var email: String?
+    var is_active: Bool?
+    var is_superuser: Bool?
+    var occupation: Occupations?
+    var work_model: WorkModels?
+    var hobbies: [Hobbies]?
+    var interests: [Interests]?
+    var uuid: String?
+}
+
+struct Pictures: Codable {
+    var uuid: String?
+    var picture: String?
+    var name: String?
+    var description: String?
+    var is_active: Bool?
+}
+
+struct CreditWallet: Codable {
+    var wp_account_id: Int?
+    var corporate_name: String?
+    var credit_amount: Double?
+}
+
+struct UserParameters: Codable {
+    var name: String
+    var phone_number: String
+    var email: String
+    var password: String
+}
+
+struct UpdateUserParameters: Codable {
+    var name: String
+    var phone_number: String
+    var email: String
+    var is_active: Bool
+    var is_superuser: Bool
+    var occupation: Occupations
+    var work_model: WorkModels
+    var hobbies: [Hobbies]
+    var interests: [Interests]
+}
+
+struct UpdatePasswordParameters: Codable {
+    var previous_password: String
+    var new_password: String
+}
+
+struct ValidateUserParameter: Codable {
+    var code: String
+}
+    
