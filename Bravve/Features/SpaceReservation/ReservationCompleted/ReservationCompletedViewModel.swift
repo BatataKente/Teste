9f24375fa5346cd54fd036eb8773f8045fefabb6
Thiago Arthur Fernandes <thiago.fernandes@foursys.com.br>
2022-08-30 17:29:19 +0000

    [BRAV-0000] Update ReservationCompleted to ViewModel Architecture.

--- Bravve/Features/SpaceReservation/ReservationCompleted/ReservationCompletedViewModel.swift
+++ Bravve/Features/SpaceReservation/ReservationCompleted/ReservationCompletedViewModel.swift
@@ -5,4 +5,18 @@
 //  Created by user218260 on 7/15/22.
 //
 
-import Foundation
+import UIKit
+
+class ReservationCompletedViewModel {
+    
+    //MARK: - createLabel
+    func createLabel(placeHolderValue text: String) -> UILabel {
+        let createLabel = UILabel()
+        createLabel.font = UIFont(name: "Ubuntu-Regular", size: CGFloat(12).generateSizeForScreen)
+        createLabel.text = text
+        createLabel.backgroundColor = UIColor(named: ColorsBravve.backgroundBravve.rawValue)
+        createLabel.textColor = UIColor(named: ColorsBravve.white_white.rawValue)
+        return createLabel
+    }
+    
+}
