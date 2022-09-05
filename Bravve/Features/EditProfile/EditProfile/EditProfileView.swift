9ec701499fc45550a11a2d3b52278f1b1247a60e
Evandro Rodrigo Minamoto <evandro.minamoto@foursys.com.br>
2022-09-05 13:25:25 +0000

    [BRAV-0000] created EditProfileView with photo and button for photo editing...

--- Bravve/Features/EditProfile/EditProfile/EditProfileView.swift
+++ Bravve/Features/EditProfile/EditProfile/EditProfileView.swift
@@ -1,8 +1,300 @@
 //
-//  PerfilEdicaoView.swift
-//  Bravve
+//  ProfileEditingView.swift
+//  Teste
 //
-//  Created by user218260 on 7/15/22.
+//  Created by Josicleison on 01/09/22.
 //
 
-import Foundation
+import UIKit
+
+class EditProfileView: UIViewController {
+    
+    struct ViewElements {
+        
+        let scroll: UIScrollView, hobbiesStack: UIStackView, mattersStack: UIStackView
+    }
+    
+    private let way: UIImageView = {
+        
+        let way = UIImageView()
+        way.image = UIImage(named: ImagesBravve.wayReserv_2.rawValue)
+        
+        return way
+    }()
+    
+    private let photoView: UIImageView = {
+        
+        let photoView = UIImageView()
+        photoView.layer.masksToBounds = false
+        photoView.image = UIImage(named: ImagesBravve.photo.rawValue)
+        photoView.clipsToBounds = true
+        
+        return photoView
+    }()
+    
+    private let editButton: UIButton = {
+        
+        let editButton = UIButton()
+        editButton.setImage(UIImage(named: IconsBravve.photoBlue.rawValue),
+                            for: .normal)
+        
+        return editButton
+    }()
+    
+    private lazy var viewElements: ViewElements = {
+        
+        let backButton: UIButton = {
+            
+            let backButton = UIButton()
+            
+            var attributedTitle = NSMutableAttributedString(string: "Perfil / Editar perfil")
+            
+            let firstAttribute: [NSAttributedString.Key: Any] = [
+                
+                .font: UIFont(name: FontsBravve.regular.rawValue,
+                              size: CGFloat(16).generateSizeForScreen) as Any,
+            ]
+            let secondAttribute: [NSAttributedString.Key: Any] = [
+                
+                .font: UIFont(name: FontsBravve.medium.rawValue,
+                              size: CGFloat(16).generateSizeForScreen) as Any,
+            ]
+            
+            attributedTitle.addAttributes(firstAttribute, range: (attributedTitle.string as NSString).range(of: "Perfil / "))
+            attributedTitle.addAttributes(secondAttribute, range: (attributedTitle.string as NSString).range(of: "Editar perfil"))
+            
+            backButton.setAttributedTitle(attributedTitle, for: .normal)
+            return backButton
+        }()
+        
+        let personalDataLabel = editProfileViewModel.createLabel("Dados pessoais")
+        
+        let personalDataStack: UIStackView = {
+            
+            let numberStack = editProfileViewModel.createFieldStackView(labelText: "Número",
+                                              textFieldText: "+55 11 95585-1111",
+                                              textFieldTextColor: .gray)
+            
+            let emailStack = editProfileViewModel.createFieldStackView(labelText: "Email",
+                                             textFieldText: "ana.maria@teste.com.br",
+                                             textFieldTextColor: .gray)
+            
+            let nameStack = editProfileViewModel.createFieldStackView(labelText: "Nome Completo",
+                                            textFieldText: "Ana Maria Silva",
+                                            buttonImage: UIImage(named: IconsBravve.edit_blue.rawValue))
+            
+            let passwordStack = editProfileViewModel.createFieldStackView(labelText: "Senha",
+                                                textFieldText: "1234567",
+                                                isSecureTextEntry: true,
+                                                buttonImage: UIImage(named: IconsBravve.edit_blue.rawValue))
+            
+            let personalDataStack = UIStackView(arrangedSubviews: [numberStack, emailStack, nameStack, passwordStack])
+            editProfileViewModel.setupStack(personalDataStack)
+            
+            return personalDataStack
+        }()
+        
+        let aboutWorkLabel = editProfileViewModel.createLabel("Sobre seu trabalho")
+        
+        let aboutWorkStack: UIStackView = {
+            
+            let officeStack = editProfileViewModel.createFieldStackView(labelText: "Área",
+                                              textFieldText: "Marketing",
+                                              buttonImage: UIImage(named: IconsBravve.edit_blue.rawValue))
+            
+            let workRegimeStack = editProfileViewModel.createFieldStackView(labelText: "Regime de trabalho",
+                                              textFieldText: "Pessoa Juridica",
+                                              buttonImage: UIImage(named: IconsBravve.edit_blue.rawValue))
+            
+            let aboutWorkStack = UIStackView(arrangedSubviews: [officeStack, workRegimeStack])
+            editProfileViewModel.setupStack(aboutWorkStack)
+            
+            return aboutWorkStack
+        }()
+        
+        let aboutYouLabel = editProfileViewModel.createLabel("Sobre você")
+        
+        let hobbiesLabel = editProfileViewModel.createLabel("Hobbies", color: .buttonPink,
+                                                            UIFont(name: FontsBravve.medium.rawValue,
+                                                                   size: CGFloat(16).generateSizeForScreen))
+        let hobbiesLimitLabel = editProfileViewModel.createLabel("(Mínimo 3)",
+                                                                 color: .buttonGray,
+                                                                 UIFont(name: FontsBravve.light.rawValue,
+                                                                        size: CGFloat(12).generateSizeForScreen))
+        
+        let hobbiesStack: UIStackView = {
+            
+            let hobbiesStack = UIStackView()
+            editProfileViewModel.setupStack(hobbiesStack, alignment: .center)
+            
+            return hobbiesStack
+        }()
+        
+        let mattersLabel = editProfileViewModel.createLabel("Assuntos", color: .buttonPink,
+                                                            UIFont(name: FontsBravve.medium.rawValue,
+                                                                   size: CGFloat(16).generateSizeForScreen))
+        let mattersLimitLabel = editProfileViewModel.createLabel("(Mínimo 3)",
+                                                          color: .buttonGray,
+                                                          UIFont(name: FontsBravve.light.rawValue,
+                                                                 size: CGFloat(12).generateSizeForScreen))
+
+        let mattersStack: UIStackView = {
+            
+            let mattersStack = UIStackView()
+            editProfileViewModel.setupStack(mattersStack, alignment: .center)
+
+            return mattersStack
+        }()
+                
+        let viewToScroll = UIView()
+        viewToScroll.addSubviews([backButton, personalDataLabel, personalDataStack, aboutWorkLabel, aboutWorkStack, aboutYouLabel, hobbiesLabel, hobbiesLimitLabel, hobbiesStack, mattersLabel, mattersLimitLabel, mattersStack])
+        
+        backButton.constraintInsideTo(.top, viewToScroll)
+        backButton.constraintInsideTo(.leading, viewToScroll, CGFloat(25).generateSizeForScreen)
+        
+        personalDataLabel.constraintOutsideTo(.top, backButton,
+                                              CGFloat(25).generateSizeForScreen)
+        personalDataLabel.constraintInsideTo(.leading, backButton,
+                                             CGFloat(5).generateSizeForScreen)
+        personalDataLabel.constraintInsideTo(.trailing, viewToScroll,
+                                             CGFloat(30).generateSizeForScreen)
+        
+        personalDataStack.constraintOutsideTo(.top, personalDataLabel,
+                                              CGFloat(15).generateSizeForScreen)
+        personalDataStack.constraintInsideTo(.leading, personalDataLabel)
+        personalDataStack.constraintInsideTo(.trailing, personalDataLabel)
+        
+        aboutWorkLabel.constraintOutsideTo(.top, personalDataStack,
+                                           CGFloat(35).generateSizeForScreen)
+        aboutWorkLabel.constraintInsideTo(.leading, personalDataStack)
+        aboutWorkLabel.constraintInsideTo(.trailing, personalDataStack)
+        
+        aboutWorkStack.constraintOutsideTo(.top, aboutWorkLabel,
+                                           CGFloat(15).generateSizeForScreen)
+        aboutWorkStack.constraintInsideTo(.leading, aboutWorkLabel)
+        aboutWorkStack.constraintInsideTo(.trailing, aboutWorkLabel)
+        
+        aboutYouLabel.constraintOutsideTo(.top, aboutWorkStack,
+                                          CGFloat(15).generateSizeForScreen)
+        aboutYouLabel.constraintInsideTo(.leading, aboutWorkStack)
+        aboutYouLabel.constraintInsideTo(.trailing, aboutWorkStack)
+        
+        hobbiesLabel.constraintOutsideTo(.top, aboutYouLabel,
+                                         CGFloat(15).generateSizeForScreen)
+        hobbiesLabel.constraintInsideTo(.leading, aboutYouLabel,
+                                        CGFloat(5).generateSizeForScreen)
+        
+        hobbiesLimitLabel.constraintInsideTo(.centerY, hobbiesLabel)
+        hobbiesLimitLabel.constraintOutsideTo(.leading, hobbiesLabel,
+                                              CGFloat(15).generateSizeForScreen)
+        
+        hobbiesStack.constraintOutsideTo(.top, hobbiesLabel,
+                                         CGFloat(15).generateSizeForScreen)
+        hobbiesStack.constraintInsideTo(.leading, aboutYouLabel)
+        hobbiesStack.constraintInsideTo(.trailing, aboutYouLabel)
+        
+        mattersLabel.constraintOutsideTo(.top, hobbiesStack,
+                                         CGFloat(15).generateSizeForScreen)
+        mattersLabel.constraintInsideTo(.leading, personalDataStack)
+        
+        mattersLimitLabel.constraintInsideTo(.centerY, mattersLabel)
+        mattersLimitLabel.constraintOutsideTo(.leading, mattersLabel,
+                                              CGFloat(15).generateSizeForScreen)
+        
+        mattersStack.constraintOutsideTo(.top, mattersLabel,
+                                         CGFloat(15).generateSizeForScreen)
+        mattersStack.constraintInsideTo(.leading, hobbiesStack)
+        mattersStack.constraintInsideTo(.trailing, hobbiesStack)
+        mattersStack.constraintInsideTo(.bottom, viewToScroll,
+                                        CGFloat(15).generateSizeForScreen)
+        
+        let scrollView = UIScrollView()
+        scrollView.addSubview(viewToScroll)
+        
+        viewToScroll.constraintInsideTo(.top, scrollView.contentLayoutGuide)
+        viewToScroll.constraintInsideTo(.leading, scrollView.contentLayoutGuide)
+        viewToScroll.constraintInsideTo(.trailing, scrollView.contentLayoutGuide)
+        viewToScroll.constraintInsideTo(.bottom, scrollView.contentLayoutGuide)
+        viewToScroll.constraintInsideTo(.width, scrollView.frameLayoutGuide)
+        
+        return ViewElements(scroll: scrollView,
+                            hobbiesStack: hobbiesStack,
+                            mattersStack: mattersStack)
+    }()
+    
+    private lazy var tabBar = TabBarClosed(self)
+    
+    private let saveButton = UIButton()
+    
+    let editProfileViewModel = EditProfileViewModel()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        setupView()
+        setupDefaults()
+        setupConstraints()
+    }
+    
+    func setupView() {
+        
+        view.setToDefaultBackgroundColor()
+        view.addSubviews([way, photoView, editButton, tabBar, saveButton, viewElements.scroll])
+        
+        editProfileViewModel.delegate = self
+        
+        editProfileViewModel.setupHobbies()
+        editProfileViewModel.setupMatters()
+    }
+    
+    func setupDefaults() {
+         
+        saveButton.setToBottomButtonDefaultAbove("Salvar", above: tabBar)
+    }
+    
+    func setupConstraints() {
+        
+        way.constraintInsideTo(.top, view)
+        way.constraintInsideTo(.trailing, view.safeAreaLayoutGuide)
+        way.heightAnchorInSuperview(CGFloat(150).generateSizeForScreen)
+        way.widthAnchorInSuperview(CGFloat(144).generateSizeForScreen)
+        
+        photoView.constraintInsideTo(.centerX, view.safeAreaLayoutGuide)
+        photoView.constraintInsideTo(.top, view,
+                                     CGFloat(40).generateSizeForScreen)
+        photoView.sizeAnchorInSuperview(view.frame.size.height/5)
+        photoView.layer.cornerRadius = view.frame.size.height/10
+        
+        editButton.sizeAnchorInSuperview(CGFloat(32).generateSizeForScreen)
+        editButton.constraintInsideTo(.centerX, photoView, view.frame.size.height/15)
+        editButton.constraintInsideTo(.centerY, photoView, view.frame.size.height/15)
+        
+        viewElements.scroll.constraintOutsideTo(.top, photoView,
+                                       CGFloat(30).generateSizeForScreen)
+        viewElements.scroll.constraintInsideTo(.leading, view)
+        viewElements.scroll.constraintInsideTo(.trailing, view)
+        viewElements.scroll.constraintOutsideTo(.bottom, saveButton)
+        
+        tabBar.constraintInsideTo(.leading, view)
+        tabBar.constraintInsideTo(.trailing, view)
+        tabBar.constraintInsideTo(.bottom, view.safeAreaLayoutGuide)
+    }
+    
+    override var prefersStatusBarHidden: Bool {true}
+}
+
+extension EditProfileView: EditProfileViewModelProtocol {
+    
+    func setHobbiesStack(_ hobbies: [String]) {
+        
+        let hobbiesStacks = editProfileViewModel.createStackViews(createCapsuleButtons(hobbies))
+        viewElements.hobbiesStack.addArrangedSubviews(hobbiesStacks)
+    }
+    
+    func setMattersStack(_ matters: [String]) {
+        
+        let mattersStacks = editProfileViewModel.createStackViews(createCapsuleButtons(matters))
+        viewElements.mattersStack.addArrangedSubviews(mattersStacks)
+    }
+}
--- Bravve/Features/EditProfile/EditProfile/EditProfileViewModel.swift
+++ Bravve/Features/EditProfile/EditProfile/EditProfileViewModel.swift
@@ -5,4 +5,165 @@
 //  Created by user218260 on 7/15/22.
 //
 
-import Foundation
+import UIKit
+
+protocol EditProfileViewModelProtocol {
+    
+    func setHobbiesStack(_ hobbies: [String])
+    func setMattersStack(_ matters: [String])
+}
+
+class EditProfileViewModel {
+    
+    private let exampleApiArray_1: [String] = ["a","bola","casa","dedo","elefante","faca","g","h","i","jaca","k","l","marmanjo","ouro","panaca","q"]
+    
+    private let exampleApiArray_2: [String] = ["a","b","cabelo","dado","esfaqueado","f","gol","h","i","j","k","lero","manga","o","p","queijo"]
+    
+    var delegate: EditProfileViewModelProtocol?
+    
+    //MARK: - createLabel
+    func createLabel(_ text: String,
+                     color: ColorsBravve = .label,
+                     _ font: UIFont? = UIFont(name: FontsBravve.medium.rawValue,
+                                          size: CGFloat(20).generateSizeForScreen)) -> UILabel {
+        
+        let label = UILabel()
+        
+        label.text = text
+        label.textColor = UIColor(named: color.rawValue)
+        label.textAlignment = .left
+        label.font = font
+        
+        return label
+    }
+    
+    func setupStack(_ stack: UIStackView, alignment: UIStackView.Alignment? = nil) {
+        
+        stack.axis = .vertical
+        stack.spacing = CGFloat(15).generateSizeForScreen
+        if let alignment = alignment {stack.alignment = alignment}
+    }
+    
+    //MARK: - createFieldStackView
+    func createFieldStackView(labelText: String,
+                              textFieldText: String,
+                              textFieldTextColor: UIColor? = UIColor(named: ColorsBravve.label.rawValue),
+                              isSecureTextEntry: Bool = false,
+                              buttonImage: UIImage? = nil) -> UIStackView {
+        
+        let verticalMargins = CGFloat(12).generateSizeForScreen
+        let horizontalMargins = CGFloat(17).generateSizeForScreen
+        
+        let label = UILabel()
+        label.text = labelText
+        label.textColor = UIColor(named: ColorsBravve.textField.rawValue)
+        label.font = UIFont(name: FontsBravve.light.rawValue,
+                            size: CGFloat(11).generateSizeForScreen)
+        
+        let textField = UITextField()
+        textField.text = textFieldText
+        textField.textColor = textFieldTextColor
+        textField.isEnabled = false
+        textField.font = UIFont(name: FontsBravve.medium.rawValue,
+                                size: CGFloat(15).generateSizeForScreen)
+        textField.isSecureTextEntry = isSecureTextEntry
+        
+        let stackView = UIStackView(arrangedSubviews: [label, textField])
+        stackView.axis = .vertical
+        stackView.spacing = CGFloat(5).generateSizeForScreen
+        stackView.backgroundColor = UIColor(named: ColorsBravve.backgroundTerms.rawValue)
+        stackView.layer.cornerRadius = CGFloat(8).generateSizeForScreen
+        
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: verticalMargins,
+                                               left: horizontalMargins,
+                                               bottom: verticalMargins,
+                                               right: horizontalMargins)
+        
+        if let buttonImage = buttonImage {
+            
+            let button = UIButton()
+            button.setImage(buttonImage, for: .normal)
+            stackView.addSubview(button)
+            
+            let handler = {(action: UIAction) in
+                
+                textField.isEnabled = !textField.isEnabled
+                
+                if textField.isEnabled {
+                    
+                    stackView.setBottomBorderBlue(color: UIColor.blue.cgColor)
+                }
+                else {
+                    
+                    stackView.setBottomBorderOnlyWithDefault(color: UIColor.black.cgColor)
+                }
+            }
+            
+            button.addAction(UIAction(handler: handler), for: .touchUpInside)
+            
+            if textField.isSecureTextEntry {
+                
+                let secureTextEntryHandler = {(action: UIAction) in
+                    
+                    textField.isSecureTextEntry = !textField.isSecureTextEntry
+                }
+                
+                button.addAction(UIAction(handler: secureTextEntryHandler), for: .touchUpInside)
+            }
+            
+            button.constraintInsideTo(.centerY, stackView)
+            button.constraintInsideTo(.trailing, stackView, CGFloat(15).generateSizeForScreen)
+        }
+        
+        return stackView
+    }
+    
+    //MARK: - setupStackView
+    func createStackViews(_ buttons: [UIButton]) -> [UIStackView] {
+        
+        let createStackView = {(_ views: [UIView]) -> UIStackView in
+            
+            let stackView = UIStackView(arrangedSubviews: views)
+            
+            stackView.spacing = 4
+            stackView.backgroundColor = .white
+            stackView.axis = .horizontal
+            stackView.distribution = .fillProportionally
+            
+            return stackView
+        }
+        
+        var stackViews = [UIStackView]()
+        
+        var from = 0
+        
+        if buttons.count%2 != 0 {
+            
+            stackViews.append(createStackView([buttons[from]]))
+            
+            from += 1
+        }
+        
+        for i in stride(from: from,
+                        to: buttons.count - 1,
+                        by: 2) {
+            
+            stackViews.append(createStackView([buttons[i],
+                                               buttons[i+1]]))
+        }
+        
+        return stackViews
+    }
+    
+    func setupHobbies() {
+        
+        delegate?.setHobbiesStack(exampleApiArray_1)
+    }
+    
+    func setupMatters() {
+        
+        delegate?.setMattersStack(exampleApiArray_2)
+    }
+}
+ 
