0cca80ba9749df47fbbf44f571fcb246b67b60fe
Kaue de Assis Jacyntho <kaue.jacyntho@foursys.com.br>
2022-09-14 18:48:10 +0000

    [BRAV-0000] Feature/update singleton

--- Bravve/Features/MyReservations/MyBooking/MyBookingModel.swift
+++ Bravve/Features/MyReservations/MyBooking/MyBookingModel.swift
@@ -9,7 +9,11 @@
 struct UserReservations {
     static var reservations: [Reservations] = []
     static var reservationsHistory: [Reservations] = []
+    static var reservationCheckOutHour: String? = nil
     static var reservationID: Int? = nil
+    static var reservationHistoryID: Int? = nil
     static var spaceID: Int? = nil
+    static var spaceHistoryID: Int? = nil
     static var spaceDetail: SpaceDetail? = nil
+    static var spaceHistoryDetail: SpaceDetail? = nil
 }
