eed4903db4a74c615c1c8247c09dfd1231607657
Daniel Junqueira Oliveira <daniel.oliveira@foursys.com.br>
2022-09-15 15:04:11 +0000

    [BRAV-0000] Updated DetailsClosedView, updated cell with native constraints,...

--- Bravve/Features/Home/DetailsClosed/DetailsClosedCollectionViewCell.swift
+++ Bravve/Features/Home/DetailsClosed/DetailsClosedCollectionViewCell.swift
@@ -13,51 +13,66 @@ class DetailsClosedCollectionViewCell: UICollectionViewCell {
         
         let imageView = UIImageView()
         imageView.backgroundColor = .darkGray
-        imageView.layer.cornerRadius = 25
+        imageView.layer.cornerRadius = CGFloat(25).generateSizeForScreen
         imageView.clipsToBounds = true
         
         return imageView
-    }()
-    
-    lazy var titleView: UIView = {
-        
-        let titleLabel = UILabel()
-        titleLabel.textColor = .white
-        titleLabel.font = UIFont(name: FontsBravve.light.rawValue,
-                                 size: CGFloat(13).generateSizeForScreen)
-        titleLabel.text = "WORKPASS"
-        
-        let titleView = UIView()
-        titleView.addSubview(titleLabel)
-        titleView.backgroundColor = UIColor(named: ColorsBravve.boxOffice.rawValue)
-        
-        titleLabel.constraintInsideTo(.top, titleView,
-                                      CGFloat(2.5).generateSizeForScreen)
-        titleLabel.constraintInsideTo(.leading, titleView,
-                                      CGFloat(2.5).generateSizeForScreen)
-        titleLabel.constraintInsideTo(.trailing, titleView,
-                                      CGFloat(2.5).generateSizeForScreen)
-        titleLabel.constraintInsideTo(.bottom, titleView,
-                                      CGFloat(2.5).generateSizeForScreen)
-        
-        return titleView
     }()
     
     override init(frame: CGRect) {
         
         super.init(frame: frame)
         
-        contentView.addSubviews([imageView, titleView])
+        contentView.addSubview(imageView)
         
-        imageView.fillSuperview()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
-        titleView.constraintInsideTo(.top, imageView,
-                                     CGFloat(25).generateSizeForScreen)
-        titleView.constraintInsideTo(.leading, imageView,
-                                     CGFloat(25).generateSizeForScreen)
+        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
+        
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createWorkPassLabel() {
+        
+        let titleView: UIView = {
+            
+            let titleLabel = UILabel()
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont(name: FontsBravve.light.rawValue,
+                                     size: CGFloat(13).generateSizeForScreen)
+            titleLabel.text = "WORKPASS"
+            
+            let titleView = UIView()
+            titleView.addSubview(titleLabel)
+            titleView.backgroundColor = UIColor(named: ColorsBravve.buttonPink.rawValue)
+            
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            titleLabel.topAnchor.constraint(equalTo: titleView.topAnchor,
+                                            constant: CGFloat(2.5).generateSizeForScreen).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: titleView.leadingAnchor,
+                                                constant: CGFloat(2.5).generateSizeForScreen).isActive = true
+            titleLabel.trailingAnchor.constraint(equalTo: titleView.trailingAnchor,
+                                                 constant: CGFloat(-2.5).generateSizeForScreen).isActive = true
+            titleLabel.bottomAnchor.constraint(equalTo: titleView.bottomAnchor,
+                                               constant: CGFloat(-2.5).generateSizeForScreen).isActive = true
+            
+            return titleView
+        }()
+        
+        contentView.addSubview(titleView)
+        
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleView.topAnchor.constraint(equalTo: imageView.topAnchor,
+                                       constant: CGFloat(25).generateSizeForScreen).isActive = true
+        titleView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor,
+                                       constant: CGFloat(25).generateSizeForScreen).isActive = true
     }
 }
