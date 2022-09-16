26947a020db0aef80fd572568e0f0136e2629a66
Daniel Junqueira Oliveira <daniel.oliveira@foursys.com.br>
2022-09-16 15:01:23 +0000

    [BRAV-0000] added proportion to tags capsule buttons

--- Bravve/Features/Home/Details Open/DetailsOpenView.swift
+++ Bravve/Features/Home/Details Open/DetailsOpenView.swift
@@ -219,6 +219,7 @@ class OpenDetailsView: UIViewController {
         
         let tagsStackView = UIStackView(arrangedSubviews: buttons)
         tagsStackView.spacing = CGFloat(5).generateSizeForScreen
+        tagsStackView.distribution = .fillProportionally
         
         let briefingLabel = detailsOpenViewModel.createLabel("""
         Sentiu o cheirinho de café e já veio aquela energia extra? Pois é bem assim que a gente se sente neste espaço: renovado. Se é porquê a decoração suave e orgânica nos deixa mais concentrados ou por conta do silêncio da localização você que vai nos dizer.
